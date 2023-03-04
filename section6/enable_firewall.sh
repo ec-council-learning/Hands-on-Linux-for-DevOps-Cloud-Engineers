22469db42b86219e9ef10657dd945b4557ae5808
Bradley Smith <bradleydsmith@gmail.com>
2023-03-04 09:03:26 -0700

    enable port 80 in fw

--- section6/enable_firewall.sh
+++ section6/enable_firewall.sh
@@ -6,4 +6,5 @@ sudo iptables -P INPUT DROP
 sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT 
 sudo iptables -A INPUT -i lo -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 sudo iptables -A INPUT -j DROP
